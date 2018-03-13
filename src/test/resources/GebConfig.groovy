ffdb9b110ebdc9071fabd3bc1e778574bc583a63
Travis Spencer <travis@curity.io>
2018-03-13 17:18:23 +0100

    fixed browser tests

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,10 +1,10 @@
-import org.openqa.selenium.firefox.FirefoxDriver
-import org.openqa.selenium.firefox.FirefoxProfile
+import com.google.common.collect.ImmutableList
+import com.google.common.collect.ImmutableMap
+import org.openqa.selenium.firefox.*
 import org.openqa.selenium.remote.UnreachableBrowserException
 
 driver = {
     def firefoxProfile = new FirefoxProfile()
-
     [
             "intl.accept_languages"                     : "en",
             "toolkit.telemetry.reportingpolicy.firstRun": "false",
@@ -14,8 +14,20 @@ driver = {
     ].each {
         k, v -> firefoxProfile.setPreference(k, v)
     }
+    File driverDirectory = new File(System.getProperty("webdriver.gecko.driverDir"))
+    String os = System.properties['os.name'].toLowerCase().contains("mac") ? "mac" : "linux"
+    String driverName = "geckodriver-$os-64bit"
+    File driverFile = new File(driverDirectory, driverName)
 
-    def testDriver = new FirefoxDriver(firefoxProfile)
+    GeckoDriverService service = new GeckoDriverService(driverFile, 4444,
+            ImmutableList.of("--log=fatal"), ImmutableMap.of());
+    FirefoxOptions options = new FirefoxOptions()
+
+    service.start();
+    options.profile = firefoxProfile
+    options.setLogLevel(FirefoxDriverLogLevel.ERROR)
+
+    def testDriver = new FirefoxDriver(service, options)
 
     Runtime.addShutdownHook {
         try {
@@ -25,12 +37,9 @@ driver = {
         }
     }
 
-    testDriver.manage().window().maximize()
-
     return testDriver
 }
 
-//baseUrl = "https://localhost:8443"
+baseUrl = "https://localhost:8443"
 
 reportsDir = new File("target/geb-reports")
-//reportOnTestFailureOnly = true
