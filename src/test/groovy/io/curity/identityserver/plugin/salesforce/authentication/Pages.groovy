72f14026b59822104e77dcb6d3313e13cec93fe3
Travis Spencer <travis@curity.io>
2018-03-14 11:01:34 +0100

    add a test that sets a certain scope and handles consent

--- src/test/groovy/io/curity/identityserver/plugin/salesforce/authentication/Pages.groovy
+++ src/test/groovy/io/curity/identityserver/plugin/salesforce/authentication/Pages.groovy
@@ -1,6 +1,8 @@
 package io.curity.identityserver.plugin.salesforce.authentication
 
 import geb.Page
+import groovy.json.JsonException
+import groovy.json.JsonSlurper
 
 class StartLoginPage extends Page {
     static url = "/test/authn/salesforce1"
@@ -10,7 +12,7 @@ class SalesforceLoginPage extends Page {
     static url = "https://login.salesforce.com"
 
     static at = {
-        title.endsWith("| Salesforce")
+        title.endsWith("Login | Salesforce")
     }
     static content = {
         usernameField { $("#username") }
@@ -23,6 +25,43 @@ class SalesforceLoginPage extends Page {
     void login(username, password) {
         usernameField = username
         passwordField = password
+        waitFor { loginButton.displayed }
         loginButton.click()
+    }
+}
+
+class SalesforceConsentPage extends Page {
+    static url = "https://eu12.salesforce.com"
+
+    static at = {
+        title == "Allow Access? | Salesforce"
+    }
+
+    static content = {
+        allowButton { $("#oaapprove") }
+    }
+
+    void allow() {
+        waitFor { allowButton.displayed }
+        allowButton.click()
+    }
+}
+
+class LoginDonePage extends Page {
+
+    def jsonBody
+
+    static at = {
+        def body = $("body")?.text()
+
+        try {
+            return body != null && (jsonBody = new JsonSlurper().parseText(body)) != null
+        } catch (JsonException e) {
+            throw new AssertionError("The response body was not valid JSON: $body")
+        }
+    }
+
+    static content = {
+        subject { jsonBody.subject }
     }
 }
--- src/test/groovy/io/curity/identityserver/plugin/salesforce/authentication/SalesforceAuthenticationIT.groovy
+++ src/test/groovy/io/curity/identityserver/plugin/salesforce/authentication/SalesforceAuthenticationIT.groovy
@@ -7,16 +7,19 @@ import spock.lang.Shared
 
 import static io.curity.identityserver.plugin.test.TestRequirements.isEnvironmentVariableSet
 import static io.curity.identityserver.plugin.test.TestRequirements.isIdshAvailable
+import static java.lang.System.getenv
 
 @Requires( { isIdshAvailable && isEnvironmentVariableSet("SALESFORCE_CLIENT_SECRET") })
 class SalesforceAuthenticationIT extends GebReportingSpec {
     @Shared
     Idsh idsh = new Idsh()
 
+    private static final SALESFORCE_AUTHENTICATOR_PATH = "profiles profile test-authentication-profile authentication-service settings authentication-service authenticators authenticator salesforce1 salesforce"
+
     def setupSpec() {
-        def salesforceClientSecret = System.getenv("SALESFORCE_CLIENT_SECRET")
+        def salesforceClientSecret = getenv("SALESFORCE_CLIENT_SECRET")
         idsh.loadTestConfig("/test-config.xml", """
-            set profiles profile test-authentication-profile authentication-service settings authentication-service authenticators authenticator salesforce1 salesforce client-secret $salesforceClientSecret
+            set $SALESFORCE_AUTHENTICATOR_PATH client-secret $salesforceClientSecret 
         """)
     }
 
@@ -30,5 +33,38 @@ class SalesforceAuthenticationIT extends GebReportingSpec {
 
         then: "we're automatically redirected to the Salesforce login page"
         at SalesforceLoginPage
+    }
+
+    @Requires({ isEnvironmentVariableSet("SALESFORCE_USERNAME") &&
+            isEnvironmentVariableSet("SALESFORCE_PASSWORD") })
+    def "Set some scope"() {
+        given:
+        def userName = getenv("SALESFORCE_USERNAME")
+        def password = getenv("SALESFORCE_PASSWORD")
+        idsh.setValue("$SALESFORCE_AUTHENTICATOR_PATH full", true)
+
+        when: "go to login page"
+        to StartLoginPage, serviceProviderId: "se.curity"
+
+        and:
+        at SalesforceLoginPage
+
+        then:
+        assert page instanceof SalesforceLoginPage
+
+        when:
+        page.login(userName, password)
+
+        then:
+        if (isAt(SalesforceConsentPage)) {
+            assert page instanceof SalesforceConsentPage
+
+            page.allow()
+        }
+
+        then:
+        waitFor { at LoginDonePage }
+        assert page instanceof LoginDonePage
+        page.subject == "teddie.curity@gmail.com"
     }
 }
